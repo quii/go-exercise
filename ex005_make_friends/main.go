7dbdb848de37b4ede5e13ac8c3186a93838c5e13
Chris James <qui666@gmail.com>
2016-10-01 19:01:20 +0100

    making friends

--- ex005_make_friends/main.go
+++ ex005_make_friends/main.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
+type person struct{
+	name string
+	friends []*person
+}
+
+func makeFriends(a, b *person){
+	a.friends = append(a.friends, b)
+	b.friends = append(b.friends, a)
+}
+
+func listFriends(p *person) (friends []string) {
+	for _, friend := range p.friends{
+		friends = append(friends, friend.name)
+	}
+	return
+}
+
 func main() {
-	// your code here
+	chris := &person{"Chris", nil}
+	ruth := &person{"Ruth", nil}
+	dan := &person{"Dan", nil}
+
+	makeFriends(chris, ruth)
+	makeFriends(dan, chris)
+
+	fmt.Println(listFriends(chris))
+	fmt.Println(listFriends(ruth))
 }
